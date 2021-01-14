26d792c1fc0ba607659562e6b9dd5fedc02472eb
Xin Luo <xin.luo@shopee.com>
2021-01-14 14:06:04 +0800

    optimize

--- cache.go
+++ cache.go
@@ -2,7 +2,8 @@ package levelcache
 
 import (
 	"context"
-	"errors"
+	"fmt"
+	"github.com/bsm/redislock"
 	"github.com/go-redis/redis/v8"
 	"github.com/jinzhu/copier"
 	jsoniter "github.com/json-iterator/go"
@@ -13,13 +14,11 @@ import (
 )
 
 const (
-	cacheKeyJoint        = "#$#"
-	cacheInvalidInterval = 24 * time.Hour
-	maxUpdateLen         = 1000
-)
-
-var (
-	DataLoaderNotFound = errors.New("data loader not found")
+	cacheKeyJoint               = "#$#"
+	defaultCacheInvalidInterval = 1440 * time.Minute //one day
+	defaultCleanupInterval      = 1441 * time.Minute
+	defaultMaxUpdateBuffer      = 100
+	defaultUpdateLockInterval   = time.Minute
 )
 
 type (
@@ -31,40 +30,70 @@ type (
 		version map[string]int64
 		updates chan string
 		stop    chan struct{}
+		locker  *redislock.Client
 	}
 
 	CacheConfig struct {
 		RedisAddr       string
 		RedisDb         int
-		RedisPassw      string
+		RedisPassword   string
 		RedisPoolSize   int
-		LocalExpiration time.Duration
+		CacheExpiration time.Duration
 		CleanupInterval time.Duration
+		LockInterval    time.Duration
+		MaxUpdateBuffer int
 	}
 )
 
+func (p *CacheConfig) checkAndLoadDefault() error {
+	if p.RedisAddr == "" {
+		return fmt.Errorf("invalid redis connect addr")
+	}
+	if p.RedisPoolSize == 0 {
+		p.RedisPoolSize = 40
+	}
+	if p.CacheExpiration == 0 {
+		p.CacheExpiration = defaultCacheInvalidInterval
+	}
+	if p.CleanupInterval == 0 {
+		p.CleanupInterval = defaultCleanupInterval
+	}
+	if p.LockInterval == 0 {
+		p.LockInterval = defaultUpdateLockInterval
+	}
+	if p.MaxUpdateBuffer == 0 {
+		p.MaxUpdateBuffer = defaultMaxUpdateBuffer
+	}
+	return nil
+}
+
 func New(cfg CacheConfig) (*levelCache, error) {
 	lc := &levelCache{
-		c:       cache.New(cfg.LocalExpiration, cfg.CleanupInterval),
+		c:       cache.New(cfg.CacheExpiration, cfg.CleanupInterval),
 		loaders: make(map[string]DataLoader),
 		cfg:     cfg,
 		version: make(map[string]int64),
-		updates: make(chan string, maxUpdateLen),
+		updates: make(chan string, cfg.MaxUpdateBuffer),
 		stop:    make(chan struct{}, 1),
 	}
 	rdb := redis.NewClient(&redis.Options{
 		Addr:     cfg.RedisAddr,
-		Password: cfg.RedisPassw,
+		Password: cfg.RedisPassword,
 		DB:       cfg.RedisDb,
 	})
 	if err := rdb.Ping(context.TODO()).Err(); err != nil {
 		return nil, err
 	}
+	lc.locker = redislock.New(rdb)
 	return lc, nil
 }
 
-func (p *levelCache) RegisterLoader(namespace string, loader DataLoader) {
+func (p *levelCache) RegisterLoader(namespace string, loader DataLoader) error {
+	if _, ok := p.loaders[namespace]; ok {
+		return fmt.Errorf("data loader [%s] existed", namespace)
+	}
 	p.loaders[namespace] = loader
+	return nil
 }
 
 func (p *levelCache) RegisterLoaders(loaders map[string]DataLoader) {
@@ -90,6 +119,10 @@ func (p *levelCache) Start(ctx context.Context) {
 	}()
 }
 
+func (p *levelCache) Stop() {
+	p.stop <- struct{}{}
+}
+
 func (p *levelCache) Get(ctx context.Context, key string, obj Cacheable) error {
 	p.checkCacheUpdate(ctx, obj.Namespace(), key)
 	return p.get(ctx, key, obj)
@@ -98,8 +131,11 @@ func (p *levelCache) Get(ctx context.Context, key string, obj Cacheable) error {
 func (p *levelCache) get(ctx context.Context, key string, obj Cacheable) error {
 	k := jointKey(obj.Namespace(), key)
 	// read local cache
-	if data, ok := p.c.Get(k); ok {
-		return copier.Copy(obj, data)
+	if content, ok := p.c.Get(k); ok {
+		if err := jsoniter.UnmarshalFromString(content.(string), obj); err != nil {
+			return err
+		}
+		return nil
 	}
 
 	// read redis cache
@@ -111,12 +147,12 @@ func (p *levelCache) get(ctx context.Context, key string, obj Cacheable) error {
 		if err := jsoniter.UnmarshalFromString(content, obj); err != nil {
 			return err
 		}
-		p.c.Set(k, obj, cacheInvalidInterval)
+		p.c.SetDefault(k, jsonString(obj))
 	}
 
 	loader, exist := p.loaders[obj.Namespace()]
 	if !exist {
-		return DataLoaderNotFound
+		return fmt.Errorf("data loader [%s] not found", obj.Namespace())
 	}
 	data, err := loader(ctx, key)
 	if err != nil {
@@ -125,8 +161,8 @@ func (p *levelCache) get(ctx context.Context, key string, obj Cacheable) error {
 	if err := copier.Copy(obj, data); err != nil {
 		return err
 	}
-	p.rdb.Set(ctx, k, jsonString(obj), cacheInvalidInterval)
-	p.c.Set(k, obj, cacheInvalidInterval)
+	p.rdb.Set(ctx, k, jsonString(obj), p.cfg.CacheExpiration)
+	p.c.SetDefault(k, jsonString(obj))
 	if _, ok := p.version[k]; !ok {
 		p.version[k] = 0
 	}
@@ -153,16 +189,11 @@ func (p *levelCache) checkCacheUpdate(ctx context.Context, namespace, key string
 }
 
 func (p *levelCache) parseAndDo(ctx context.Context, k string) error {
-	namespace, key := splitKey(k)
-	loader, exist := p.loaders[namespace]
-	if !exist {
-		return DataLoaderNotFound
-	}
-	data, err := loader(ctx, key)
+	content, err := p.rdb.Get(ctx, k).Result()
 	if err != nil {
 		return err
 	}
-	p.c.Set(k, data, cacheInvalidInterval)
+	p.c.SetDefault(k, content)
 	return nil
 }
 
@@ -171,29 +202,34 @@ func (p *levelCache) Refresh(ctx context.Context, namespace, key string) {
 	if !exist {
 		return
 	}
-	data, err := loader(ctx, key)
-	if err != nil {
-		return
-	}
-	k := jointKey(namespace, key)
-	p.rdb.Set(ctx, k, jsonString(data), cacheInvalidInterval)
-	p.c.Set(k, data, cacheInvalidInterval)
+	go func() {
+		k := jointKey(namespace, key)
+		lockKey := jointKey("lock", k)
+		for {
+			lock, err := p.locker.Obtain(ctx, lockKey, p.cfg.LockInterval, nil)
+			if err != nil {
+				time.Sleep(time.Millisecond)
+				continue
+			}
 
-	vk := jointKey("version", k)
-	if recNo, err := p.rdb.Incr(ctx, vk).Result(); err == nil {
-		p.version[k] = recNo
-	}
+			data, err := loader(ctx, key)
+			if err != nil {
+				return
+			}
+			p.c.SetDefault(k, jsonString(data))
+			p.rdb.Set(ctx, k, jsonString(data), p.cfg.CacheExpiration)
+			vk := jointKey("version", k)
+			if recNo, err := p.rdb.Incr(ctx, vk).Result(); err == nil {
+				p.version[k] = recNo
+			}
+			_ = lock.Release(ctx)
+			break
+		}
+	}()
 }
 
 func jointKey(a ...string) string {
 	return strings.Join(a, cacheKeyJoint)
-}
-func splitKey(a string) (string, string) {
-	arr := strings.Split(a, cacheKeyJoint)
-	if len(arr) < 2 {
-		return "", ""
-	}
-	return arr[0], arr[1]
 }
 func jsonString(obj interface{}) string {
 	content, err := jsoniter.MarshalToString(obj)
